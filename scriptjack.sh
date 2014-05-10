2e9a18fc27a5b0d58b01fc1c86f9e669329b02c8
artur <arturbmallmann@gmail.com>
2014-05-10 07:06:57 -0300

    hora de ir dormir

--- scriptjack.sh
+++ scriptjack.sh
@@ -2,22 +2,39 @@
 DIR=$HOME/configuracoesJack
 
 PATCHBAY=${DIR}/definicoesqjack.xml
-LOGJACK=${DIR}/logqjack
-
-LOGJAMIN=${DIR}/logjamin
+LOGJACK=${DIR}/.logs/logqjack
+LOGA2JM=${DIR}/.logs/loga2jmidid
+LOGJAMIN=${DIR}/.logs/logjamin
 JAMINSETUP=${DIR}/.jamin/default.jam
+
+MPCONFIG=${HOME}/.mplayer/config
+
+function ativaconfigs {
+		ln -s ${DIR}/asoundrc ${HOME}/.asoundrc
+		mv ${MPCONFIG} ${MPCONFIG}old
+		ln -s ${DIR}/mplayer ${MPCONFIG}
+}
+function desativaconfigs {
+		rm ${MPCONFIG}
+		cp ${MPCONFIG}old ${MPCONFIG}
+		rm ~/.asoundrc
+}
+
 case $1 in
 	start)
 		qjackctl -a ${PATCHBAY} -s &> ${LOGJACK}&
+		a2jmidid &> ${LOGA2JM} &
 		jamin -f ${JAMINSETUP} &> ${LOGJAMIN}&
 		jamin-scene 1&
-		ln -s ${DIR}/asoundrc ~/.asoundrc
+		ativaconfigs
 		;;
 	stop)
 		kill -9 `pidof jamin` &>/dev/null
 		kill -9 `pidof qjackctl.real` &>/dev/null
+		kill -9 `pidof a2jmidid` &> /dev/null
 		jack_control stop
-		rm ~/.asoundrc;;
+		desativaconfigs
+		;;
 	config)
 		mv ~/.jackdrc ~/.jackdrcold
 		mv ~/.jamin ~/.jaminold
@@ -26,4 +43,3 @@ case $1 in
 #		--help) echo "help";
 		*) echo "Please enter a option: $0 start|stop|config";;
 esac
-
